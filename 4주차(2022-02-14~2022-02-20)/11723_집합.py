3c62343b817a25876c2fa6d74540e5032d6c6bfe
JunSeokCheon <goosw775@naver.com>
2022-02-16 12:47:29 +0000

    4주차

--- 4주차(2022-02-14~2022-02-20)/11723_집합.py
+++ 4주차(2022-02-14~2022-02-20)/11723_집합.py
@@ -6,26 +6,26 @@ for _ in range(int(sys.stdin.readline())):
     if len(command) == 2:
         oper = command[0]
         x = int(command[1])
+
+        if oper == "add":
+          if x not in data:
+            data.append(x)
+        elif oper == "remove":
+            if x in data:
+                data.remove(x)
+        elif oper == "check":
+            if x not in data:
+                print(0)
+            else:
+                print(1)
+        elif oper == "toggle":
+            if x not in data:
+                data.append(x)
+            else:
+                data.remove(x)
     else:
         oper = command[0]
-
-    if oper == "add":
-        if x not in data:
-            data.append(x)
-    elif oper == "remove":
-        if x in data:
-            data.remove(x)
-    elif oper == "check":
-        if x not in data:
-            print(0)
-        else:
-            print(1)
-    elif oper == "toggle":
-        if x not in data:
-            data.append(x)
-        else:
-            data.remove(x)
-    elif oper == "all":
-        data = [i for i in range(1, 21)]
-    elif oper == "empty":
-        data = []
+        if oper == "all":
+          data = [i for i in range(1, 21)]
+        elif oper == "empty":
+          data = []
