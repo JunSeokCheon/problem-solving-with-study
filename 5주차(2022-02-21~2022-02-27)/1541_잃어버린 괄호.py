8be1a26680177ef57f743436dc98ac0bb7192fd1
JunSeokCheon <goosw775@naver.com>
2022-02-23 05:53:50 +0000

    5주차

--- 5주차(2022-02-21~2022-02-27)/1541_잃어버린 괄호.py
+++ 5주차(2022-02-21~2022-02-27)/1541_잃어버린 괄호.py
@@ -1,20 +1,24 @@
-# 삽질 한거..(결국 실패: 이유 몰?루)
-# cal = input()
+# 삽질 한거..(결국 실패: 이유 몰?루) 성공-> eval 을 쓰면 수가 0으로 시작할 수 있는 오류때매. 예제에서는 맞아도 제출 시 런타임 에러(문법 오류)가 생긴다.
+cal = input()
 # print(cal.split("-"))
-# cal_split = cal.split("-")
-# result = 0
-# for i, str in enumerate(cal_split):
-#   if "+" in str:
-#     cal_split[i] = eval(str) # eval 은 식(문자열로 된 식, 단일 숫자도 식이다)을 실행하는 함수 | 예외 "0000009"은 되지 않는다. 왜냐하면 파이썬에서는 10진법에서는 숫자 맨 앞에 0을 쓸 수 없다.
-#   else:
-#     cal_split[i] = int(str)
+cal_split = cal.split("-")
+result = 0
 
+for i, str in enumerate(cal_split):
+  semi_sum = 0
+  if "+" in str:
+    for num in str.split("+"):
+      semi_sum += int(num)
+    cal_split[i] = semi_sum
+      # cal_split[i] = eval(str) # eval 은 식(문자열로 된 식, 단일 숫자도 식이다)을 실행하는 함수 | 예외 "0000009"은 되지 않는다. 왜냐하면 파이썬에서는 10진법에서는 숫자 맨 앞에 0을 쓸 수 없다.  
+  else:
+    cal_split[i] = int(str)
 # print(cal_split)
-# result += cal_split[0]
-# for i in range(1, len(cal_split)):
-#   result -= cal_split[i]
+result += cal_split[0]
+for i in range(1, len(cal_split)):
+  result -= cal_split[i]
 
-# print(result)
+print(result)
 ######## 삽질 : 만약 -20+30+40-10+20-30 이 온다면 ?
 # *핵심* 
 # 1. -가 나올 때 다음에 -가 나오면 그대로 처리, +가 나오면 묶어준다.
@@ -25,15 +29,15 @@
 # ex2.2. -20+30+40-10+20-30 -> -(20+30+40)-(10+20)-30
 # ex3.1. 25-35+15-25-30+15-20+30+40 -> 25-(35+15)-(25-30+15)-(20+30+40)
 # ex3.2. 25+35+15-25-15+25-30+40-30+50 -> 25+35+15-25-(15+25)-(30+40)-(30+50)
-cal = input()
-sum = 0
-cal_split = cal.split("-")
+# cal = input()
+# sum = 0
+# cal_split = cal.split("-")
 
-for num in cal_split[0].split("+"):
-  sum += int(num)
+# for num in cal_split[0].split("+"):
+#   sum += int(num)
 
-for num in cal_split[1:]:
-  for num_split in num.split("+"):
-    sum -= int(num_split)
+# for num in cal_split[1:]:
+#   for num_split in num.split("+"):
+#     sum -= int(num_split)
 
-print(sum)
+# print(sum)
