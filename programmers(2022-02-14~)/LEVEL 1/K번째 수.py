33ac968e2242d2f2d9a2a4fdbc8b5c0cecb71b4b
JunSeokCheon <goosw775@naver.com>
2022-02-25 10:09:33 +0000

    프로그래머스

---
+++ programmers(2022-02-14~)/LEVEL 1/K번째 수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        new_array = array[command[0]-1:command[1]]
+        new_array.sort()
+        answer.append(new_array[command[2]-1])
+        
+    print(answer)
+    return answer
