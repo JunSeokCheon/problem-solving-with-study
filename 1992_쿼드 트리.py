0101847c59cd935f77e796e23b2902be5f19055d
JunSeokCheon <goosw775@naver.com>
2022-02-26 08:14:33 +0000

    6주차

---
+++ 1992_쿼드 트리.py
@@ -0,0 +1,25 @@
+# 무조건 1->2->3->4사 분면으로 이동
+n = int(input())
+n_list = [list(map(int,input())) for _ in range(n)]
+
+def quad(x,y,n):
+  check = n_list[x][y]
+
+  for i in range(x,x+n):
+    for j in range(y,y+n):
+      if check != n_list[i][j]:
+        print("(", end="")
+        quad(x,y,n//2)  # 1사분
+        quad(x,y+n//2,n//2) # 2사분
+        quad(x+n//2,y,n//2) # 3사분
+        quad(x+n//2,y+n//2,n//2) # 4사분
+        print(")", end="")
+        return
+  if check == 1:
+    print("1", end="")
+    return
+  else:
+    print("0", end="")
+    return
+
+quad(0,0,n)
