bdc6d7e8717eb3627216f266fbc24e58b571db86
JunSeokCheon <goosw775@naver.com>
2022-03-22 16:03:52 +0000

    프로그래머스

---
+++ programmers(2022-02-14~)/LEVEL 1/다트게임.py
@@ -0,0 +1,29 @@
+def solution(dartResult):
+    num = ''
+    resultlist = []
+    for i in dartResult:
+        if i.isdigit():
+            num += i
+        else:
+            if i == "S":
+                num = int(num) ** 1
+                resultlist.append(num)
+                num = ''
+            elif i == "D":
+                num = int(num) ** 2
+                resultlist.append(num)
+                num = ''
+            elif i == "T":
+                num = int(num) ** 3
+                resultlist.append(num)
+                num = ''
+            elif i == "*":
+                if len(resultlist) > 1:
+                    resultlist[-1] = resultlist[-1] * 2
+                    resultlist[-2] = resultlist[-2] * 2
+                else:
+                    resultlist[-1] = resultlist[-1] * 2
+            elif i == "#":
+                resultlist[-1] = resultlist[-1] * (-1)
+        
+    return sum(resultlist)
