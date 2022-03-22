09c345382d15ee5294260a5eb395f78e661f5c91
JunSeokCheon <goosw775@naver.com>
2022-03-22 16:16:53 +0000

    프로그래머스

--- programmers(2022-02-14~)/LEVEL 1/다트게임.py
+++ programmers(2022-02-14~)/LEVEL 1/다트게임.py
@@ -1,6 +1,8 @@
+# 다트게임 - 문자열을 잘라서 숫사형태이면 값을 저장하고 문자라면 연산 후 list에 넣고, 값 초기화| # *, # 일 때는 리스트 인덱스 -1, -2 사용 | 중요한 점 - 10의 처리를 위해 int처리보다 str처리 |
 def solution(dartResult):
     num = ''
     resultlist = []
+  
     for i in dartResult:
         if i.isdigit():
             num += i
