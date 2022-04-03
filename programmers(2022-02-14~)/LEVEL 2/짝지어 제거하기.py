628ed14356ec5052494dc682e5b1815b16a3e202
JunSeokCheon <goosw775@naver.com>
2022-04-03 02:00:55 +0000

    level 2

---
+++ programmers(2022-02-14~)/LEVEL 2/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = -1
+    stack = []
+    
+    for alpha in s:
+        if len(stack) != 0 and stack[-1] == alpha:
+            stack.pop()
+        else:
+            stack.append(alpha)
+    
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
