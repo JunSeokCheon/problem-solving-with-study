cd4c68a7442defb8ec93f72f06460f6c59b49477
JunSeokCheon <goosw775@naver.com>
2022-08-05 06:43:33 +0000

    프로그래머스 level 1

---
+++ programmers(2022-02-14~)/LEVEL 1/가운데 글자 가져오기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+
+    if len(s)%2 == 0:
+        answer += s[len(s)//2-1]
+        answer += s[len(s)//2]
+    else:
+        answer += s[len(s)//2]
+    
+    return answer
